5d8ba278224b1549ecffeeae746e63a35f7e5d07
Hanisha Patel <razaa1960@uhcl.edu>
2023-10-26 16:13:39 -0500

    first commit

---
+++ automated_content_creation/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css'; 
+
+const App = () => {
+  return (
+    <>
+      <h1>Automated Content Creation</h1>
+      <div className="navbar">
+          <a href="/">Home</a>
+          <a href="/scheduling">Scheduling</a>
+          <a href="/analytics">Analytics</a>
+         
+      </div>
+    </>
+  );
+};
+
+export default App;
