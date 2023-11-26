2def16363aa090fefb4279bb6da17f750a893aa7
Hanisha Patel <145165090+AsadRaza05@users.noreply.github.com>
2023-11-25 22:05:57 -0600

    updated analytics

--- automated_content_creation/src/Analytics.js
+++ automated_content_creation/src/Analytics.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from 'react-router-dom'; 
 import './App.css';
 
@@ -12,6 +12,23 @@ const Analytics = () => {
 
   }
 
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Fetch data from the API
+    fetch("https://api.seniorproject.xyz/api/videos/test%40gmail.com")
+      .then((response) => response.json())
+      .then((result) => {
+        setData(result);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <div>
       <h1>Automated Content Creation</h1>
@@ -21,6 +38,34 @@ const Analytics = () => {
         <a href="/Analytics">Analytics</a>
         <button  className="logout-button" onClick={logout}>Logout</button>
       </div>
+
+      <div>
+    {loading ? (
+      <p>Loading...</p>
+    ) : Array.isArray(data) && data.length > 0 ? (
+      <table>
+        <thead>
+          <tr>
+            <th>Video Type</th>
+            <th>Scheduled Time</th>
+            <th>Download Link</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item) => (
+            <tr key={item.id}>
+              <td>{item.videoType}</td>
+              <td>{item.scheduledTime}</td>
+              <td>{item.videoUrl}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    ) : (
+      <p>No data available</p>
+    )}
+  </div>
+
     </div>
   );
 }
